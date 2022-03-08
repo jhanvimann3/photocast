6340b340c60da8f56805465f50cf1f6f754aae54
winnyv <58780983+winnyv@users.noreply.github.com>
2022-03-08 13:10:39 +0000

    Update Humidity.js

--- src/components/Humidity.js
+++ src/components/Humidity.js
@@ -1,14 +1,12 @@
-import {AiOutlineCloud} from "react-icons/ai"
-
 const Humidity = ({humidity,visibility,cloud}) => {
     return (
     <div id="float-container">
-        <div id="float-child"> 
-            <div class="humidity">Humidity: {humidity} <br></br>
-            Visibility: {visibility}</div>  
+        <div id="float-child">
+            <div class="humidity">Humidity: <b>{humidity}</b> <br></br>
+            Visibility: <b>{visibility}M</b></div>  
         </div>
         <div id="float-child">
-            <div class="cloud"><AiOutlineCloud/>Cloud cover:<br></br><b>{cloud}</b></div>
+            <div class="cloud">Cloud cover:<br></br><b>{cloud}</b></div>
             <br></br><br></br>
         </div>
     </div>
